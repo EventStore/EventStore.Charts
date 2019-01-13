aaa1da7e150217722c3c45414d60cec7d2d1e8c9
Andrew Meier <ameier38@gmail.com>
2019-01-13 16:25:09 -0500

    feature/test: move into eventstore dir

--- build.fsx
+++ build.fsx
@@ -6,7 +6,33 @@ nuget Fake.Core.Target //"
 
 open Fake.Core
 
+module Path =
+    let toPosix path =
+        path
+        |> String.replace "\\" "/"
+
+let buildDir = __SOURCE_DIRECTORY__ |> Path.toPosix
+let lintImage = "quay.io/helmpack/chart-testing"
+let lintImageTag = "latest"
+
+let exec command args =
+    let exitCode = Shell.Exec(command, args)
+    if exitCode <> 0 then failwithf "%s %s failed" command args
+
 Target.create "Default" (fun _ ->
     Trace.trace "EventStore.Charts")
+
+Target.create "Lint" (fun _ ->
+    Trace.trace "Linting chart..."
+    let runArgs = sprintf "--rm -v \"%s:/workdir\"" buildDir
+    let imageArg = sprintf "%s:%s" lintImage lintImageTag
+    let lintCommand = "ct lint --charts /workdir/eventstore"
+    let dockerArgs = sprintf "run %s %s %s" runArgs imageArg lintCommand
+    exec "docker" dockerArgs)
+
+open Fake.Core.TargetOperators
+
+"Default"
+ ==> "Lint"
 
 Target.runOrDefault "Default"
